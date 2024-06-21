6dfbf5b1aba1129a38f757f93297f202e339d1ab
AzizKitten <97047609+AzizKitten@users.noreply.github.com>
2024-06-21 10:25:02 +0100

    Update app.py

--- app.py
+++ app.py
@@ -155,11 +155,6 @@ def integrate(integrand, lower_limit: float, upper_limit: float) -> float:
         x_i = lower_limit + i * segment_width
         result += integrand(x_i)
     result *= segment_width
-    if result >= 1e10:
-        return float('inf')
-    elif result <= -1e10:
-        return float('-inf')
-    return result
 
 def factorial(x: float) -> float:
     """
